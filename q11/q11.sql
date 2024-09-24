257c28c399f84cc2f5b6b2be95934cb3797e3645
mieshiii <mieseliel.delacruz@gmail.com>
2024-09-24 20:44:54 +0800

    #10 - add q11 solution

--- q11/q11.sql
+++ q11/q11.sql
@@ -61,3 +61,7 @@
 -- | Dan  | 500   |
 -- +------+-------+
 
+-- solution
+SELECT Employee.name,Bonus.bonus FROM Employee 
+LEFT JOIN Bonus ON Employee.empID = Bonus.empID
+WHERE bonus < 1000 OR Bonus IS NULL ;
