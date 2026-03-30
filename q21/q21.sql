6659720238fce232ce09490b8cac0c3919743e41
mieshiii <mieseliel.delacruz@gmail.com>
2026-03-31 00:14:56 +0400

    #28 - add correct solution and notes for q21

--- q21/q21.sql
+++ q21/q21.sql
@@ -63,3 +63,25 @@ FROM
 Delivery
 GROUP BY customer_id
 ORDER BY order_date DESC
+
+-- correct solution
+SELECT 
+    ROUND(
+        100.0 * AVG(CASE WHEN order_date = customer_pref_delivery_date THEN 1.0 ELSE 0.0 END),
+        2
+    ) AS immediate_percentage
+FROM (
+    SELECT 
+        customer_id,
+        order_date,
+        customer_pref_delivery_date,
+        ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date) AS rn
+    FROM 
+        Delivery
+) AS FirstOrders
+WHERE 
+    rn = 1;
+
+-- notes
+-- the use of a subquery is required to filter the First orders as it requires a separate aggregation for the orders
+-- then you aggregate the percentage off of that aggregated dataset hence the subquery
