b0d087ec1e8d8b92d86c05b0f46c66af00eda0b9
mieshiii <mieseliel.delacruz@gmail.com>
2026-04-23 23:48:55 +0400

    #45 - add q33 solution, correct solution and notes

--- q33/q33.sql
+++ q33/q33.sql
@@ -41,3 +41,38 @@
 -- +-----------------+
 -- Explanation: 1 is the only number that appears consecutively for at least three times.
 
+-- my draft solution
+WITH row_numbers AS
+    SELECT
+        id,
+        num,
+        ROW_NUMBER() OVER (
+        PARTITION BY num
+        ORDER BY id ASC
+    ) AS row_num
+    FROM Logs
+
+SELECT 
+    num AS ConsecutiveNums
+FROM 
+Logs
+GROUP BY num
+HAVING COUNT(row_num) >= 3
+
+-- correct solution using lag or lead
+SELECT DISTINCT num AS ConsecutiveNums
+FROM (
+    SELECT 
+        num,
+        LAG(num, 1) OVER (ORDER BY id) AS prev_num,
+        LAG(num, 2) OVER (ORDER BY id) AS prev_prev_num
+    FROM Logs
+) t
+WHERE num = prev_num AND num = prev_prev_num;
+
+-- notes:
+-- my solution has an issue is that ROW_NUMBER() assigns sequential numbers within each partition, 
+-- but this doesn't help identify consecutive occurrences in the original sequence
+
+-- correct solution uses lag/lead to check the prev 2 or next 2 then check the 3 columns
+-- being the original, next/prev 1, next/prev 2 if they are all equal
